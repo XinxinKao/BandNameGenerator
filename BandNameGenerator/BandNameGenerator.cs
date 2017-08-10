229b2ba5d9ee7d684b4e26e09919261106ece335
xinyi <xinyi.kao@titansoft.com.sg>
2017-08-10 18:52:44 +0800

    add project

---
+++ BandNameGenerator/BandNameGenerator.cs
@@ -0,0 +1,22 @@
+﻿namespace BandNameGenerator
+{
+    public class BandNameGenerator
+    {
+        public string Generator(string inputString)
+        {
+            if (string.IsNullOrEmpty(inputString))
+                return null;
+
+            var lowerInputString = inputString.Trim().ToLower();
+            var head = lowerInputString.Substring(0,1).ToUpper();
+            var body = lowerInputString.Substring(1);
+            var text = head + body;
+            if (lowerInputString.Substring(0, 1) == lowerInputString.Substring(lowerInputString.Length - 1))
+            {
+                return text + body;
+            }
+
+            return "The " + text;
+        }
+    }
+}
---
+++ BandNameGenerator/BandNameGeneratorUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace BandNameGenerator
+{
+    [TestFixture]
+    public class BandNameGeneratorUnitTest
+    {
+        [TestCase("The Knife", "knife")]
+        [TestCase("Tartart", "tart")]
+        [TestCase("Sandlesandles", "sandles")]
+        [TestCase("The Bed", "bed")]
+        public void BandNameGeneratorTest(string expected, string inputString)
+        {
+            var nameGenerator = new BandNameGenerator();
+            var actual = nameGenerator.Generator(inputString);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
+
